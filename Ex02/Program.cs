e16603f4783816b33cf58e9fd25cc6d6cf43e8b1
Ekaterina <katya.kinal@gmail.com>
2022-08-20 12:54:36 +0300

    Ex02 новые методы добавлены

--- Ex02/Program.cs
+++ Ex02/Program.cs
@@ -41,4 +41,20 @@ for (int i = 0; i < image.GetLength(0); i++)
   }
 }
 
+void FillImage(int row, int col)
+{
+    if(pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+
+    }
+}
+
+
+PrintImage(pic);
+FillImage(13, 13);
 PrintImage(pic);
